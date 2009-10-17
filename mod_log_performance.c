63c823ba781f89dd08460627fff91a1dd84b3ad5
Amr Mostafa <alienbrain@pipeline.(none)>
2009-10-17 14:05:02 +0200

    Rely on /proc/<pid>/stat which is more widely available

--- mod_log_performance.c
+++ mod_log_performance.c
@@ -21,34 +21,49 @@
 #include <apr_optional.h>
 #include <mod_log_config.h>
 
+#include <stdlib.h>
+#include <unistd.h>
+
+static int pagesize = 1;
+
 static const char *my_rss(request_rec *r)
 {
     char *filename;
-    filename = apr_psprintf(r->pool, "/proc/%s/status", ap_append_pid(r->pool, "", ""));
+    filename = apr_psprintf(r->pool, "/proc/%s/stat", ap_append_pid(r->pool, "", ""));
 
     apr_file_t *procfile;
     if (APR_SUCCESS != apr_file_open(&procfile, filename, APR_READ, APR_OS_DEFAULT, r->pool)) {
         return "-";
     }
 
-    char *status = apr_palloc(r->pool, 1024);
-    char *rss;
-    while (apr_file_gets(status, 1024, procfile) == APR_SUCCESS) {
-        rss = strstr(status, "VmRSS:");
-        if (rss != NULL) {
-            apr_file_close(procfile);
+    static char stat[1024];
+    char *sp, *rss;
+    // Read RSS from /proc/<pid>/stat, which is the 24th field
+    while (apr_file_gets(stat, 1024, procfile) == APR_SUCCESS) {
+        // Skip until the process name, which might be parser-unfriendly
+        // This advances us 2 fields
+        sp = strchr(stat, ')');
+        sp += 2;
 
-            rss += 6;
-            while (*rss == ' ' || *rss == '\t') {
-                rss++;
+        // Skip 21 fields
+        int i;
+        for (i = 0; i < 21; i++) {
+            sp = strchr(sp, ' ');
+            if (sp == NULL) {
+                return "-";
             }
-            char *c = rss;
-            while (*c != ' ') {
-                c++;
-            }
-            *c = '\0';
-            return (const char *) rss;
+            sp++;
         }
+
+        // Read RSS
+        rss = sp;
+        while (*sp != ' ') {
+            sp++;
+        }
+        *sp = '\0';
+
+        // Convert to kB and return
+        return (const char *) apr_itoa(r->pool, (atoi(rss) * pagesize) / 1024);
     }
 
     apr_file_close(procfile);
@@ -73,8 +88,14 @@ static int mlp_pre_config(apr_pool_t *p, apr_pool_t *plog, apr_pool_t *ptemp)
     return OK;
 }
 
+static void mlp_init_child(apr_pool_t *p, server_rec *s)
+{
+    pagesize = getpagesize();
+}
+
 static void mlp_register_hooks(apr_pool_t *p) {
     ap_hook_pre_config(mlp_pre_config, NULL, NULL, APR_HOOK_REALLY_FIRST);
+    ap_hook_child_init(mlp_init_child, NULL, NULL, APR_HOOK_MIDDLE);
 }
 
 module AP_MODULE_DECLARE_DATA log_performance_module = {
